2903d02444602ef5b87b4e155e7daebea1de3d99
Aswin Augustine <aswinaugustine91@gmail.com>
2024-01-29 20:16:01 +0530

    first

--- db/index.js
+++ db/index.js
@@ -5,9 +5,9 @@ import mongoose from "mongoose"
 mongoose.connect("mongodb://localhost:27017/Task-1").then(()=>{
     console.log("DB Connected")
 })
-// .catch(e=>{
-//     console.log(e)
-// })
+.catch(e=>{
+    console.log(e)
+})
 
 
 export default mongoose
